76cd654b7057f52e1ea488d0d9b4d1e0d7fabc8c
Monika <moha0009@stud.kea.dk>
2026-03-27 16:34:40 +0100

    done

--- src/app/components/Watch.jsx
+++ src/app/components/Watch.jsx
@@ -1,21 +1,36 @@
+"use client";
 import Image from "next/image";
 
 const Watch = ({ currentWatch }) => {
   return (
-    <div className="relative">
-      {/* ur billedet*/}
-      <Image src="/navy.png" width={400} height={400} alt="watch" className="drop-shadow-2xl" priority />
-
-      {/* faver */}
-      <div className="absolute right-[3px] top-1/2 -translate-y-1/2 flex flex-col gap-3">
-        <button className="w-6 h-6 rounded-full bg-black border-2"></button>
-
-        <button className="w-6 h-6 rounded-full bg-[#56d6c2] border-2"></button>
-
-        <button className="w-6 h-6 rounded-full bg-[#cfdee4] border-2"></button>
+    <div>
+      <div className="relative">
+        <Image src={currentWatch} width={400} height={400} alt="watch" className="drop-shadow-2xl" priority />
       </div>
     </div>
   );
 };
 
 export default Watch;
+
+// import Image from "next/image";
+
+// const Watch = ({ currentWatch }) => {
+//   return (
+//     <div className="relative">
+//       {/* ur billedet*/}
+//       <Image src="/navy.png" width={400} height={400} alt="watch" className="drop-shadow-2xl" priority />
+
+//       {/* faver */}
+//       <div className="absolute right-[3px] top-1/2 -translate-y-1/2 flex flex-col gap-3">
+//         <button className="w-6 h-6 rounded-full bg-black border-2"></button>
+
+//         <button className="w-6 h-6 rounded-full bg-[#56d6c2] border-2"></button>
+
+//         <button className="w-6 h-6 rounded-full bg-[#cfdee4] border-2"></button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Watch;
