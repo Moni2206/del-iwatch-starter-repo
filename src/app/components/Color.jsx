528739381c08c402b322744d57ea896386091124
Monika <moha0009@stud.kea.dk>
2026-04-09 15:14:42 +0200

    Refactor Color component layout and styles; add DarkModeToggle component; update Heading and Text components for better responsiveness; modify global styles; enhance Home component with dark mode functionality

--- src/app/components/Color.jsx
+++ src/app/components/Color.jsx
@@ -4,10 +4,15 @@ import Image from "next/image";
 const Color = ({ setWatch }) => {
   return (
     <div>
-      <div className="absolute right-[3px] top-1/2 -translate-y-1/2 flex flex-col gap-3 pr-13 pb-37">
-        <button onClick={() => setWatch("/navy.png")} className="w-6 h-6 rounded-full bg-black border-2"></button>
-        <button onClick={() => setWatch("/mint.png")} className="w-6 h-6 rounded-full bg-[#56d6c2] border-2"></button>
-        <button onClick={() => setWatch("/ocean.png")} className="w-6 h-6 rounded-full bg-[#cfdee4] border-2"></button>
+      <div
+        className="  absolute 
+  right-13  
+  top-1/2 -translate-y-1/1 
+  flex flex-col gap-3 "
+      >
+        <button onClick={() => setWatch("/navy.png")} className="w-6 h-6 rounded-full bg-[#757F8F] border-2 border-white"></button>
+        <button onClick={() => setWatch("/mint.png")} className="w-6 h-6 rounded-full bg-[#56d6c2] border-2 border-white"></button>
+        <button onClick={() => setWatch("/ocean.png")} className="w-6 h-6 rounded-full bg-[#cfdee4] border-2 border-white"></button>
       </div>
 
       <section className="flex gap-4 justify-end mt-6">
